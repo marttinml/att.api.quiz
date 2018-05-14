c382c5ff11b18dda8ad138b6bc1f76c5b5713ade
Donas4Ceos <ciber_donas123@hotmail.com>
2018-05-14 15:33:52 -0500

    examen
    examen

--- app/api/v0/examen/examen.model.js
+++ app/api/v0/examen/examen.model.js
@@ -180,7 +180,7 @@ module.exports.delete = function (db, id, callback) {
 
 module.exports.calificaciones = function (db, id, callback) {
   var result = [];
-  var cursor = db.collection("responder_encuesta").find({ "encuesta.id": Number(id) });
+  var cursor = db.collection("responder_encuesta").find({ "encuesta.id": Number(id) }).project({ _id: 0, "encuesta.attuid": 1, "encuesta.nombre": 1, "nombre": "$encuesta.nombre", "encuesta.titulo": 1 });
   
   var code=200;
   cursor.each(function (err, doc) {
